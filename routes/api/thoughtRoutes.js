02fcc769216b8e944ddee05c71f798c20affa10e
LeVente Berry <berrylevente@gmail.com>
2024-05-09 01:51:43 -0400

    setup routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Thought, User } = require('../../models');
+
+// GET all thoughts
+
+// GET a single thought by _id
+
+// POST to create a thought, and add created thought _id to associated user's thoughts array
+
+// PUT (update) a thought by _id
+
+// DELETE a thought by _id
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Thought, User } = require('../../models');
+
+// GET all users
+
+// GET a single user by its _id and populated thought and friend data
+
+// POST a new user
+
+// PUT (update) a user by its _id
+
+// DELETE a user by its _id
+
+// POST to add a new friend to a users friend list
+
+// DELETE to remove a friend from a user's friend list
+
+module.exports = router;
