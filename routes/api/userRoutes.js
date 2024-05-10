18767b89e34695134b40dfa887dbd21c38e7c219
LeVente Berry <berrylevente@gmail.com>
2024-05-10 12:10:04 -0400

    adding base structure of user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Thought, User } = require('../../models');
 
 // GET all users
-router.get('/getUsers', async (req, res) => {
+router.get('/allUsers ', async (req, res) => {
     try {
         const users = await User.find(); 
     } catch (error) {
@@ -12,23 +12,63 @@ router.get('/getUsers', async (req, res) => {
 });
 
 // GET a single user by its _id and populated thought and friend data
-router.get('/getUser/:id', async (req, res) => {
+router.get('/user/:id', async (req, res) => {
     try {
-        
+        //get a  single user by id
     } catch (error) {
         console.error(error);
-        res.status(500).json({message: "Unable to obtain user info."});
+        res.status(500).json({message: `Unable to obtain user info. Error: ${error}`});
     }
 });
 
 // POST a new user
+router.post('/user/:id', async (req, res) => {
+    try {
+        // Add new user
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: `Unable add a new user. Error: ${error}`});
+    }
+});
 
 // PUT (update) a user by its _id
+router.put('/user/:id', async (req, res) => {
+    try {
+        // update user by id
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: `Unable to update user. Error: ${error}`});
+    }
+});
 
 // DELETE a user by its _id
+router.delete('/user/:id', async (req, res) => {
+    try {
+        // delete user by id
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: `Unable to delete user. Error: ${error}`});
+    }
+});
 
 // POST to add a new friend to a users friend list
+router.post('/addFriend/:id', async (req, res) => {
+    try {
+        // Add friend to specific friend list
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: `Unable add friend to friends list. Error: ${error}`});
+    }
+});
 
 // DELETE to remove a friend from a user's friend list
+router.delete('/removeFriend/:id', async (req, res) => {
+    try {
+        // delete user from friends list
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: `Unable to remove friend from friends list. Error: ${error}`});
+    }
+});
 
 module.exports = router;
