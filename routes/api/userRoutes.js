79d8f5f1981500eb420d54f5249140f164eaed04
LeVente Berry <berrylevente@gmail.com>
2024-05-10 11:07:55 -0400

    basic route for user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,8 +2,24 @@ const router = require('express').Router();
 const { Thought, User } = require('../../models');
 
 // GET all users
+router.get('/getUsers', async (req, res) => {
+    try {
+        const users = await User.find(); 
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: `Unable to obtain all users. Error: ${error}`});
+    }
+});
 
 // GET a single user by its _id and populated thought and friend data
+router.get('/getUser/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Unable to obtain user info."});
+    }
+});
 
 // POST a new user
 
